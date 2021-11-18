80c54ce51ed0fa39125f126198752b68f8e924e5
Pyki <py.gil.ludovic@outlook.com>
2021-11-18 18:43:27 +0100

    feat(views): add views show computer

--- src/resources/views/computers/show.blade.php
+++ src/resources/views/computers/show.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Reference n° {{ $computer->serial_number }}</h2>
+                <h2></h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('computers.index') }}"> Back</a>
@@ -15,15 +15,64 @@
         </div>
     </div>
 
+    <!--Section: Block Content-->
+    <section class="mb-5">
+
+        <div class="row">
+            <div class="col-md-6 mb-4 mb-md-0">
+
+                <div id="mdb-lightbox-ui"></div>
+
+                <div class="mdb-lightbox">
+
+                    <div class="row product-gallery mx-1">
+
+                        <div class="col-12 mb-0">
+                            <figure class="view overlay rounded z-depth-1 main-img">
+
+                                <img src="{{ $computer->picture }}" class="img-fluid z-depth-1">
+
+                            </figure>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <h5>Reference n° {{ $computer->serial_number }}</h5>
 
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Marque:</strong>
-                {{ $computer->brand->name }}
+                <p class="pt-1">{{ $computer->comment }}</p>
+                <div class="table-responsive">
+                    <table class="table table-sm table-borderless mb-0">
+                        <tbody>
+                            <tr>
+                                <th class="pl-0 w-25" scope="row"><strong>Marque</strong></th>
+                                <td>{{ $computer->brand->name }}</td>
+                            </tr>
+                            <tr>
+                                <th class="pl-0 w-25" scope="row"><strong>Disponible</strong></th>
+                                <td>{{ $computer->is_avaible }}</td>
+                            </tr>
+                            <tr>
+                                <th class="pl-0 w-25" scope="row"><strong>Delivery</strong></th>
+                                <td>USA, Europe</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <hr>
+
+
+                <button type="button" class="btn btn-primary btn-md mr-1 mb-2">Réserver</button>
             </div>
         </div>
-    </div>
+
+    </section>
+    <!--Section: Block Content-->
 
 @endsection
