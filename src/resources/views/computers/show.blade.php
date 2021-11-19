bf1ea3dcb2ff0cd16a5bcb47b4ae52d0513ebb83
Pyki <py.gil.ludovic@outlook.com>
2021-11-19 13:54:05 +0100

    feat(views): add views show computer

--- src/resources/views/computers/show.blade.php
+++ src/resources/views/computers/show.blade.php
@@ -6,9 +6,7 @@
 
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2></h2>
-            </div>
+
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('computers.index') }}"> Back</a>
             </div>
@@ -20,33 +18,20 @@
 
         <div class="row">
             <div class="col-md-6 mb-4 mb-md-0">
-
                 <div id="mdb-lightbox-ui"></div>
-
                 <div class="mdb-lightbox">
-
                     <div class="row product-gallery mx-1">
-
                         <div class="col-12 mb-0">
                             <figure class="view overlay rounded z-depth-1 main-img">
-
                                 <img src="{{ $computer->picture }}" class="img-fluid z-depth-1">
-
                             </figure>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
             <div class="col-md-6">
-
                 <h5>Reference n° {{ $computer->serial_number }}</h5>
 
-
-                <p class="pt-1">{{ $computer->comment }}</p>
                 <div class="table-responsive">
                     <table class="table table-sm table-borderless mb-0">
                         <tbody>
@@ -59,8 +44,8 @@
                                 <td>{{ $computer->is_avaible }}</td>
                             </tr>
                             <tr>
-                                <th class="pl-0 w-25" scope="row"><strong>Delivery</strong></th>
-                                <td>USA, Europe</td>
+                                <th class="pl-0 w-25" scope="row"><strong>Description</strong></th>
+                                <td>{{ $computer->comment }}</td>
                             </tr>
                         </tbody>
                     </table>
